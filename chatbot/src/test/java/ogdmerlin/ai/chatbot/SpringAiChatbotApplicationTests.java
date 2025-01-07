a508ba71cf6170f3c1d97cf9acc708a916f192c8
OG DMERLIN <ogdmerlin@gmail.com>
2025-01-07 21:18:22 +0000

    update: Boilerplate

---
+++ chatbot/src/test/java/ogdmerlin/ai/chatbot/SpringAiChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ogdmerlin.ai.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
